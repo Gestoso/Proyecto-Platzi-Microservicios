13e646fbdd2586996bd748a3cb87979513e62876
Albert <gestosoalbert@gmail.com>
2025-10-16 23:56:59 +0200

    Verificación del token JWT

--- auth/index.js
+++ auth/index.js
@@ -1,9 +1,43 @@
 const jwt = require('jsonwebtoken');
+const config = require('../config');
+const secret = config.jwt.secret;
+const secure = require('../api/components/user/secure');
 
 function sign(data) {
     return jwt.sign(data, 'secreto');
 }
+const check = {
+    own: function(req, owner) {
+        const decoded = decodeHeader(req);
+        console.log(decoded); 
+    },
+}
 
+function verify(token) {
+    return jwt.verify(token, secret)
+}
+
+function getToken(auth) {
+    if (!auth) {
+        throw new Error('No viene token');
+    }
+
+    if (auth.indexOf('Bearer ') === -1) {
+        throw new Error('Token invalido');
+    }
+
+    let token = auth.replace('Bearer ', '');
+
+    return token;
+}
+
+function decodeHeader(req) {
+    const authorization = req.headers.authorization || '';
+    const token = getToken(authorization);
+    const decoded = verify(token);
+
+    req.user = decoded;
+}
 module.exports = {
     sign
 };
